2023c76c334d4cc8720643a25913370ba3356734
AmirPhpDeveloper <amiralimahmoodi8913@gmail.com>
2025-08-09 08:44:54 +0200

    [Learning] Update: Introduced to documentation basics

--- scripts.js
+++ scripts.js
@@ -1,25 +1,6 @@
-//Short arrow functions
-
-let sum = (num) => num + 2;
-
-console.log(sum(12));
-
-//
-
-let sum2 = (num1, num2) => num1 + num2;
-
-console.log(sum2(12, 13));
-
-// back ticks
-
-const sayHello = (name) => `hello MR ${name}`;
-console.log(sayHello("DevMahmoodi"));
-
-//
-
-const sayHi = (name, family) => {
-  return `Welcome 
-    ${name} ${family} `;
-};
-
-console.log(sayHi("mahdi", "mahmoodi"));
+// document
+function sayHello() {
+  let el = document.querySelector("#welcome");
+  el.style.color = "green";
+  el.textContent = "welcome to my repo";
+}
