c0bb5a5f3a08dcd71538efa3095e9459a8645a71
AmirPhpDeveloper <amiralimahmoodi8913@gmail.com>
2025-08-09 07:45:04 +0200

    [Learning] Update: Implemented backticks for template literals in JavaScript

--- scripts.js
+++ scripts.js
@@ -9,3 +9,17 @@ console.log(sum(12));
 let sum2 = (num1, num2) => num1 + num2;
 
 console.log(sum2(12, 13));
+
+// back ticks
+
+const sayHello = (name) => `hello MR ${name}`;
+console.log(sayHello("DevMahmoodi"));
+
+//
+
+const sayHi = (name, family) => {
+  return `Welcome 
+    ${name} ${family} `;
+};
+
+console.log(sayHi("mahdi", "mahmoodi"));
