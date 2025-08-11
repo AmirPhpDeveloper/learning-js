7bada29268ec33b87768d4267bff0fa0bed48e54
AmirPhpDeveloper <amiralimahmoodi8913@gmail.com>
2025-08-11 14:23:30 +0330

    [Feature] Add: LocalStorage persistence for counter state

--- script.js
+++ script.js
@@ -2,22 +2,31 @@
 const counterVlueEl = document.querySelector(".counter_value");
 const counterButtonDecEl = document.querySelector(".counter_button--decrease");
 const counterButtonIncEl = document.querySelector(".counter_button--increase");
-const counterTitleEl = document.querySelector(".counter_title");
 const counterReseetBtnEl = document.querySelector(".counter_reset-button");
 
+// Get the counter value from localStorage or default to 0 if not found
+let counterValue = parseInt(localStorage.getItem("counterValue")) || 0;
+counterVlueEl.innerHTML = counterValue;
+
 // functions
+function updateValue(newValue) {
+  counterValue = newValue;
+  counterVlueEl.innerHTML = counterValue;
+  localStorage.setItem("counterValue", counterValue);
+}
+
 function ValueInc() {
-  counterVlueEl.innerHTML = parseInt(counterVlueEl.innerHTML) + 1;
+  updateValue(counterValue + 1);
 }
 function ValueDec() {
-  if (counterVlueEl.innerHTML != 0) {
-    counterVlueEl.innerHTML = parseInt(counterVlueEl.innerHTML) - 1;
+  if (counterValue != 0) {
+    updateValue(counterValue - 1);
   } else {
     alert("We cannot count negative numbers.");
   }
 }
 function resetValue() {
-  counterVlueEl.innerHTML = 0;
+  updateValue(0);
 }
 
 // Listeners
