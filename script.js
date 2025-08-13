75fbc7b8f3def3f9dbfd631e7be29be9e2a6c8f3
AmirPhpDeveloper <amiralimahmoodi8913@gmail.com>
2025-08-13 07:51:12 +0200

    feat(javascript): add javascript file
    - add limit for social
    - add limit style
    - block the js code in textarea

--- script.js
+++ script.js
@@ -7,8 +7,41 @@ const facebookNumberEl = document.querySelector(".stat__number--facebook");
 
 // Functions
 const countCharacters = () => {
-  const numberOfCharacters = textareaEl.value.length;
+  let numberOfCharacters = textareaEl.value.length;
+  let numberOfWords = textareaEl.value.split(" ").length;
+  let numberOfTwitterLeft = 280 - textareaEl.value.length;
+  let numberOfacebookLeft = 2200 - textareaEl.value.length;
+
+  if (textareaEl.value.includes("<script>")) {
+    alert("you can'nt write script in input");
+    console.log(textareaEl.value.replace("<script>", " "));
+    textareaEl.value = textareaEl.value.replace("<script>", " ");
+  }
+
+  if (textareaEl.value.length === 0) {
+    numberOfWords = 0;
+  }
+
+  if (numberOfTwitterLeft < 0) {
+    twitterNumberEl.classList.add("stat__number_limit");
+  } else {
+    {
+      twitterNumberEl.classList.remove("stat__number_limit");
+    }
+  }
+
+  if (numberOfacebookLeft < 0) {
+    facebookNumberEl.classList.add("stat__number_limit");
+  } else {
+    {
+      facebookNumberEl.classList.remove("stat__number_limit");
+    }
+  }
+
   charactersNumberEl.textContent = numberOfCharacters;
+  wordsNumberEl.textContent = numberOfWords;
+  twitterNumberEl.textContent = numberOfTwitterLeft;
+  facebookNumberEl.textContent = numberOfacebookLeft;
 };
 
 // Listeners
